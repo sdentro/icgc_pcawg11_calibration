458897a2ee49e572854f8676935569a005dc3f76
sdentro <sdentro@gmail.com>
2015-05-06 15:54:37 +0100

    Adding Titan

--- 1_purity_ploidy.R
+++ 1_purity_ploidy.R
@@ -2,6 +2,7 @@ morris_file = "data/morris/purity_ploidy.txt"
 vanloo_wedge_file = "data/vanloo_wedge/1_purity_ploidy/purity_ploidy.txt"
 peifer_file = "data/peifer/Purity_Ploidy/ICGC_Pilot63_Purity_Ploidy.txt"
 sahinalp_file = "data/sahinalp/citup_pilot63/citup_purity_ploidy_v1.txt"
+broad_file = "data/broad/titan_purity_ploidy.txt"
 
 source("code/Parser.R")
 source("code/Plotting.R")
@@ -12,8 +13,10 @@ peifer = parse.purity.ploidy.peifer(peifer_file, vanloo_wedge$sample)
 sahinalp = parse.purity.ploidy(sahinalp_file)
 # Replace 0 with NA
 sahinalp$ploidy = rep(NA, nrow(sahinalp))
-list_of_tables = list(morris, vanloo_wedge, peifer, sahinalp)
-vector_of_names = c("morris", "vanloo_wedge", "peifer", "sahinalp")
+broad = parse.purity.ploidy(broad_file)
+colnames(broad) = c("sample", "purity", "ploidy")
+list_of_tables = list(morris, vanloo_wedge, peifer, sahinalp, broad)
+vector_of_names = c("morris", "vanloo_wedge", "peifer", "sahinalp", "broad")
 
 #' Convert a list of tables into a matrix with purity estimates only, placing NAs where a group has not reported anything
 create.purity.table = function(list_of_tables, vector_of_names) {
