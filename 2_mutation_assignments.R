fa9d91e8a65aa9efb16828b11c09ad517f894132
sdentro <sdentro@gmail.com>
2015-05-06 17:29:37 +0100

    Work around no results of a method when creating mutation master table

--- 2_mutation_assignments.R
+++ 2_mutation_assignments.R
@@ -63,6 +63,8 @@ get.mut.assigned.by.who = function(master_table, list_of_tables, vector_of_names
       is_assigned = sapply(1:nrow(sub), function(i, sub, num.cols) { !any(is.na(sub[i,3:num.cols])) }, sub=sub, num.cols=num.cols)
       sub = sub[is_assigned,]
       master_table = cbind(master_table, (master_table[,1] %in% sub[,1] & master_table[,2] %in% sub[,2]))
+    } else {
+      master_table = cbind(master_table, rep(F, nrow(master_table)))
     }
   }
   colnames(master_table) = c("Chromosome", "Position", vector_of_names)
